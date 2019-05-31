4e905a332abef5517de9ec8bffbfa2b444c4f8df
bartmika <bart@mikasoftware.com>
2019-05-30 21:50:58 -0400

    Added better validation error messages to login validator.

--- src/validators/loginValidator.js
+++ src/validators/loginValidator.js
@@ -10,9 +10,15 @@ export default function validateInput(data) {
 
     if (data.email === undefined || data.email === null || validator.isEmpty(data.email) || data.email === "") {
         errors.email = 'This field is required';
+        errors.nonFieldErrors = "Please fill in the email";
     }
     if (data.password === undefined || data.password === null || validator.isEmpty(data.password) || data.password === "") {
         errors.password = 'This field is required';
+        if (data.email === undefined || data.email === null || validator.isEmpty(data.email) || data.email === "") {
+            errors.nonFieldErrors = "Please fill in the emai and password";
+        } else {
+            errors.nonFieldErrors = "Please fill in the password";
+        }
     }
     return {
         errors,
