4c9078201d659f244147ceec8a6a712cb859ba3d
bartmika <bart@mikasoftware.com>
2019-06-02 15:43:46 -0400

    More icons added.

--- src/components/dashboard/staffDashboardComponent.js
+++ src/components/dashboard/staffDashboardComponent.js
@@ -49,14 +49,14 @@ class StaffDashboardComponent extends Component {
                         </section>
 
                         <div className="jumbotron">
-                            <h1 className="display-4">Announcements</h1>
+                            <h1 className="display-4"><i className="fas fa-bullhorn"></i>&nbsp;Announcements</h1>
                             <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
                                 attention to featured content or information.</p>
                             <hr className="my-4" />
                             <p>It uses utility classes for typography and spacing to space content out within the larger
                                 container.</p>
                             <p className="lead">
-                                <Link className="btn btn-primary btn-lg" to="#">Learn more</Link>
+                                <Link className="btn btn-primary btn-lg" to="#">Learn more&nbsp;<i className="fas fa-chevron-right"></i></Link>
                             </p>
                         </div>
 
