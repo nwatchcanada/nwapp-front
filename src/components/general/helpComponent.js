138541cd91a172561794c5adf1a73a6928f936be
bartmika <bart@mikasoftware.com>
2019-06-18 17:07:46 -0400

    Implemented help page. #69

--- src/components/general/helpComponent.js
+++ src/components/general/helpComponent.js
@@ -13,23 +13,29 @@ class HelpComponent extends Component {
                         <li className="breadcrumb-item">
                            <Link to="/dashboard"><i className="fas fa-tachometer-alt"></i>&nbsp;Dashboard</Link>
                         </li>
-                        <li className="breadcrumb-item active" aria-current="page"><i className="fas fa-eye"></i>&nbsp;Help</li>
+                        <li className="breadcrumb-item active" aria-current="page"><i className="fas fa-question-circle"></i>&nbsp;Help</li>
                     </ol>
                 </nav>
 
-                <h1><i className="fas fa-eye"></i>&nbsp;Help Policy</h1>
-                <p>Last updated: November 2014</p>
 
-                <br />
-                <blockquote>Websites linking to this page have chosen to adopt this generic help policy as their own. This means that they agree to abide by the principles laid out below.</blockquote>
+                <h1><i className="fas fa-question-circle"></i>&nbsp;Help</h1>
+                { /* <p>Last updated: November 2014</p> */}
 
-                <br />
-                <br />
-
-                <h2>Information that is gathered from visitors</h2>
-                <p>In common with other websites, log files are stored on the web server saving details such as the visitor's IP address, browser type, referring page and time of visit.</p>
-                <p>Cookies may be used to remember visitor preferences when interacting with the website.</p>
-                <p>Where registration is required, the visitor's email and a username will be stored on the server.</p>
+                <div class="row my-4 pt-4">
+                    <div class="col-md-4 mx-auto">
+                        <div class="card text-center">
+                            <p class="mt-4 pt-3 mb-2"><i class="far fa-envelope fa-4x text-muted"></i></p>
+                            <div class="card-body">
+                                <h3 class="card-title">Contact</h3>
+                            </div>
+                            <ul class="list-group list-group-flush text-left">
+                                <li class="list-group-item"><i class="fas fa-envelope"></i> support@workery.ca</li>
+                                <li class="list-group-item"><i class="fas fa-phone"></i> +1(519)438-1111</li>
+                                <li class="list-group-item"><i class="fas fa-map-marker-alt"></i> London, ON Canada</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
