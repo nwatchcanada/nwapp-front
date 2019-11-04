cafa03223a55a9d03df4a39301267d66dc25a035
bartmika <bart@mikasoftware.com>
2019-11-04 10:13:52 -0500

    Upgraded Yarn and removed update button . #225

--- src/components/associates/retrieve/associateFullRetrieveComponent.js
+++ src/components/associates/retrieve/associateFullRetrieveComponent.js
@@ -271,9 +271,6 @@ export default class AssociateFullRetrieveComponent extends Component {
                         </table>
                         <form>
                             <div className="form-group">
-                                <Link to={`/associate/${slug}/update`} className="btn btn-primary btn-lg mt-4 float-right pl-4 pr-4">
-                                    <i className="fas fa-edit"></i>&nbsp;Edit
-                                </Link>
                                 <Link to={`/associates`} className="btn btn-secondary btn-lg mt-4 float-left pl-4 pr-4">
                                     <i className="fas fa-arrow-circle-left"></i>&nbsp;Back
                                 </Link>
