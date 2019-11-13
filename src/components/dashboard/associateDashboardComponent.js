dbda5bdc9c0592d4b5fb8438014e2ce77df644e3
bartmika <bart@mikasoftware.com>
2019-11-13 00:11:14 -0500

    Updated dashboard. #221

--- src/components/dashboard/associateDashboardComponent.js
+++ src/components/dashboard/associateDashboardComponent.js
@@ -10,57 +10,86 @@ class AssociateDashboardComponent extends Component {
                 <div className="d-flex align-items-stretch">
                     <main id="main">
                         <h1><i className="fas fa-tachometer-alt"></i>&nbsp;Dashboard</h1>
-                        <section className="row text-center placeholders">
-                            <div className="col-sm-3 placeholder">
-                                <div className="rounded-circle mx-auto mt-4 mb-4 circle-200 bg-pink">
-                                    <Link to="#" className="d-block link-ndecor" title="Clients">
-                                        <h1 className="circle-title">1,100</h1>
-                                    </Link>
-                                </div>
-                                <h4>Clients</h4>
-                                <div className="text-muted">View your client list</div>
-                            </div>
-                            <div className="col-sm-3 placeholder">
-                                <div className="rounded-circle mx-auto mt-4 mb-4 circle-200 bg-dgreen">
-                                    <Link to="#" className="d-block link-ndecor" title="Jobs">
-                                        <h1 className="circle-title">10,000</h1>
-                                    </Link>
-                                </div>
-                                <h4>Jobs</h4>
-                                <span className="text-muted">View your job history</span>
-                            </div>
-                            <div className="col-sm-3 placeholder">
-                                <div className="rounded-circle mx-auto mt-4 mb-4 circle-200 bg-dblue">
-                                    <Link to="#" className="d-block link-ndecor" title="Members">
-                                        <h1 className="circle-title">2,200</h1>
-                                    </Link>
-                                </div>
-                                <h4>Associate Members</h4>
-                                <span className="text-muted">View Member Data</span>
-                            </div>
-                            <div className="col-sm-3 placeholder">
-                                <div className="rounded-circle mx-auto mt-4 mb-4 circle-200 bg-orange">
-                                    <Link to="#" className="d-block link-ndecor" title="Tasks">
-                                        <h1 className="circle-title">12</h1>
-                                    </Link>
-                                </div>
-                                <h4>Tasks</h4>
-                                <span className="text-muted">View your tasks</span>
-                            </div>
-                        </section>
+                        <div className="row">
+                            <div className="col-md-12">
 
-                        <div className="jumbotron">
-                            <h1 className="display-4">Announcements</h1>
-                            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
-                                attention to featured content or information.</p>
-                            <hr className="my-4" />
-                            <p>It uses utility classes for typography and spacing to space content out within the larger
-                                container.</p>
-                            <p className="lead">
-                                <Link className="btn btn-primary btn-lg" to="#">Learn more</Link>
-                            </p>
+                                <div className="card-group row">
+                                    
+                                    <div className="col-sm-3">
+                                        <div className="card box-shadow text-center mx-auto">
+                                            <div className="card-custom-top-2">
+                                                <i className="fas fa-shield-alt fa-3x"></i>
+                                            </div>
+                                            <div className="card-body">
+                                                <h3 className="card-title">My Watches</h3>
+                                                <p className="card-text">View my watches.</p>
+                                                <Link to="/settings/districts" className="btn btn-success btn-lg">
+                                                    Go&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-sm-3">
+                                        <div className="card box-shadow text-center mx-auto">
+                                            <div className="card-custom-top-2">
+                                                <i className="fas fa-newspaper fa-3x"></i>
+                                            </div>
+                                            <div className="card-body">
+                                                <h3 className="card-title">News</h3>
+                                                <p className="card-text">View news</p>
+                                                <Link to="/settings/districts" className="btn btn-success btn-lg">
+                                                    Go&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-3">
+                                        <div className="card box-shadow text-center mx-auto">
+                                            <div className="card-custom-top-2">
+                                                <i className="fas fa-map-pin fa-3x"></i>
+                                            </div>
+                                            <div className="card-body">
+                                                <h3 className="card-title">Items</h3>
+                                                <p className="card-text">View items</p>
+                                                <Link to="/settings/districts" className="btn btn-success btn-lg">
+                                                    Go&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-3">
+                                        <div className="card box-shadow text-center mx-auto">
+                                            <div className="card-custom-top-2">
+                                                <i className="fas fa-atlas fa-3x"></i>
+                                            </div>
+                                            <div className="card-body">
+                                                <h3 className="card-title">Resources</h3>
+                                                <p className="card-text">View resources</p>
+                                                <Link to="/settings/districts" className="btn btn-success btn-lg">
+                                                    Go&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-3">
+                                        <div className="card box-shadow text-center mx-auto">
+                                            <div className="card-custom-top-2">
+                                                <i className="fas fa-donate fa-3x"></i>
+                                            </div>
+                                            <div className="card-body">
+                                                <h3 className="card-title">Donate</h3>
+                                                <p className="card-text">View donations</p>
+                                                <Link to="/settings/districts" className="btn btn-success btn-lg">
+                                                    Go&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
-
                     </main>
                 </div>
             </div>
