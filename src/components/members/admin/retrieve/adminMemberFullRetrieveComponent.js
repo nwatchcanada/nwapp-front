3a2d13347db111656601af78f46d093eab7ec0e5
bartmika <bart@mikasoftware.com>
2019-11-26 01:11:01 -0500

    Implemented more access to the retrieve API endpoint

--- src/components/members/admin/retrieve/adminMemberFullRetrieveComponent.js
+++ src/components/members/admin/retrieve/adminMemberFullRetrieveComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import Moment from 'react-moment';
+// import 'moment-timezone';
 
 import { BootstrapPageLoadingAnimation } from "../../../bootstrap/bootstrapPageLoadingAnimation";
 import { BootstrapErrorsProcessingAlert } from "../../../bootstrap/bootstrapAlert";
@@ -70,6 +72,188 @@ export default class AdminMemberFullRetrieveComponent extends Component {
                                     <th scope="row" className="bg-light">Type of Client</th>
                                     <td>{member && member.typeOfLabel}</td>
                                 </tr>
+
+                                <tr className="bg-dark">
+                                    <th scope="row" colSpan="2" className="text-light">
+                                        <i className="fas fa-phone"></i>&nbsp;Contact
+                                        <Link to={`/admin/member/${slug}/update/contact`} className="btn btn-success btn-sm  float-right pl-4 pr-4">
+                                            <i className="fas fa-edit"></i>&nbsp;
+                                        </Link>
+                                    </th>
+                                </tr>
+                                {member && member.organizationName &&
+                                    <tr>
+                                        <th scope="row" className="bg-light">Company Name</th>
+                                        <td>{member.organizationName}</td>
+                                    </tr>
+                                }
+                                <tr>
+                                    <th scope="row" className="bg-light">Full Name</th>
+                                    <td>{member.fullName}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Primary Telephone</th>
+                                    <td><a href={`tel:${member.e164PrimaryPhone}`}>{member.primaryPhone}</a></td>
+                                </tr>
+                                {member && member.secondaryPhone &&
+                                    <tr>
+                                        <th scope="row" className="bg-light">Secondary Telephone</th>
+                                        <td><a href={`tel:${member.e164SecondaryPhone}`}>{member.secondaryPhone}</a></td>
+                                    </tr>
+                                }
+                                <tr>
+                                    <th scope="row" className="bg-light">Email</th>
+                                    <td><a href={`mailto:${member.email}`}>{member.email}</a></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Ok to Email?</th>
+                                    <td>
+                                        {member.isOkToEmail
+                                            ?"Yes"
+                                            :"No"
+                                        }
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Ok to Text?</th>
+                                    <td>
+                                        {member.isOkToText
+                                            ?"Yes"
+                                            :"No"
+                                        }
+                                    </td>
+                                </tr>
+
+
+                                <tr className="bg-dark">
+                                    <th scope="row" colSpan="2" className="text-light">
+                                        <i className="fas fa-map-marker-alt"></i>&nbsp;Postal Address
+                                        <Link to={`/admin/member/${slug}/update/address`} className="btn btn-success btn-sm  float-right pl-4 pr-4">
+                                            <i className="fas fa-edit"></i>&nbsp;
+                                        </Link>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Location</th>
+                                    <td>
+                                        <a href={member.googleMapsUrl} target="_blank">{member.address}&nbsp;<i className="fas fa-external-link-alt"></i></a>
+                                    </td>
+                                </tr>
+
+
+
+                                <tr className="bg-dark">
+                                    <th scope="row" colSpan="2" className="text-light">
+                                        <i className="fas fa-chart-pie"></i>&nbsp;Metrics
+                                        <Link to={`/admin/member/${slug}/update/metrics`} className="btn btn-success btn-sm  float-right pl-4 pr-4">
+                                            <i className="fas fa-edit"></i>&nbsp;
+                                        </Link>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Date of Birth</th>
+                                    <td>
+                                        {member && member.yearOfBirth
+                                            ? member.yearOfBirth
+                                            : "-"
+                                        }
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Gender</th>
+                                    <td>{member.genderLabel}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Description</th>
+                                    <td>{member.description}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Tag(s)</th>
+                                    <td>
+                                        {member.tags && member.tags.map(
+                                            (tag) => <TagItem tag={tag} key={tag.id} />)
+                                        }
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Join Date</th>
+                                    <td>
+                                        {member && <Moment format="MM/DD/YYYY">{member.joinDate}</Moment>}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">How did they discover us?</th>
+                                    <td>{member && member.howDidYouHearLabel}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">What do you expect from NW?</th>
+                                    <td>{member && member.expectationLabel}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">What does NW mean to you?</th>
+                                    <td>{member && member.meaningLabel}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Are you willing to willing_to_volunteer as a area coordinator / associate?</th>
+                                    <td>{member && member.willingToVolunteerLabel}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Is there another member of your household which is registered with us?</th>
+                                    <td>
+                                        {member && member.anotherHouseholdMemberRegistered
+                                            ? "Yes"
+                                            : "No"
+                                        }
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">How many people are in your household?</th>
+                                    <td>{member && member.totalHouseholdCount}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">How many people in your household are under the age of 18?</th>
+                                    <td>{member && member.under18YearsHouseholdCount}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">The employee count at this member's organization</th>
+                                    <td>{member && member.organizationEmployeeCount}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">The year this organization was founded</th>
+                                    <td>{member && member.organizationFoundingYear}</td>
+                                </tr>
+
+
+
+
+                                <tr className="bg-dark">
+                                    <th scope="row" colSpan="2" className="text-light">
+                                        <i className="fas fa-server"></i>&nbsp;System
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Created At</th>
+                                    <td>
+                                        {member && <Moment format="MM/DD/YYYY hh:mm:ss a">{member.created}</Moment>}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Created By</th>
+                                    <td>{member.createdBy}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Modified At</th>
+                                    <td>
+                                        {member && <Moment format="MM/DD/YYYY hh:mm:ss a">{member.lastModified}</Moment>}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" className="bg-light">Modified By</th>
+                                    <td>{member.lastModifiedBy}</td>
+                                </tr>
+
+
+
                             </tbody>
                         </table>
                     </div>
