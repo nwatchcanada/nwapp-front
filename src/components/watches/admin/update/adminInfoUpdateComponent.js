f6deec961e90cb32f91438fdea39d613c2708da2
bartmika <bart@mikasoftware.com>
2020-01-30 22:31:38 -0500

    Prototype of the update info.

--- src/components/watches/admin/update/adminInfoUpdateComponent.js
+++ src/components/watches/admin/update/adminInfoUpdateComponent.js
@@ -1,32 +1,23 @@
 // import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import ReactModal from 'react-modal';
 
 import { BootstrapPageLoadingAnimation } from "../../../bootstrap/bootstrapPageLoadingAnimation";
 import { BootstrapErrorsProcessingAlert } from "../../../bootstrap/bootstrapAlert";
+// import { BootstrapCheckbox } from "../bootstrap/bootstrapCheckbox";
 import { BootstrapInput } from "../../../bootstrap/bootstrapInput";
 import { BootstrapSingleSelect } from "../../../bootstrap/bootstrapSingleSelect";
-import { BootstrapTelephoneInput } from "../../../bootstrap/bootstrapTelephoneInput";
-import { BootstrapRadio } from "../../../bootstrap/bootstrapRadio";
-import { BootstrapCountrySelect } from '../../../bootstrap/bootstrapCountrySelect'
-import { BootstrapRegionSelect } from '../../../bootstrap/bootstrapRegionSelect'
-import { BootstrapDatePicker } from '../../../bootstrap/bootstrapDatePicker';
-import { BootstrapTextarea } from "../../../bootstrap/bootstrapTextarea";
 import { BootstrapMultipleSelect } from "../../../bootstrap/bootstrapMultipleSelect";
-import {
-    BUSINESS_TYPE_OF,
-    RESIDENCE_TYPE_OF,
-    COMPANY_TYPE_OF_CHOICES,
-    IS_OK_TO_EMAIL_CHOICES,
-    IS_OK_TO_TEXT_CHOICES,
-} from "../../../../constants/api";
+import { BootstrapTextarea } from "../../../bootstrap/bootstrapTextarea";
 
 
 export default class AdminWatchInfoUpdateComponent extends Component {
     render() {
         const {
-            typeOf,
-            slug, errors, isLoading, onClick, onTextChange
+            tags, isTagLoading, tagOptions, name, description, district, isDistrictLoading,
+            districtOptions, errors, isLoading, onClick, onTextChange,
+            onSelectChange, onMultiChange, slug, error
         } = this.props;
         return (
             <main id="main" role="main">
@@ -48,31 +39,72 @@ export default class AdminWatchInfoUpdateComponent extends Component {
                     </ol>
                 </nav>
 
-                <h1>
-                    <i className="fas fa-edit"></i>&nbsp;Update Watch
-                </h1>
+                <h1>Update Watch - Information</h1>
 
-                <div className="row">
-                    <div className="col-md-5 mx-auto mt-2">
-                        <form>
-                            <h1><i className="fas fa-edit"></i>&nbsp;Update Watch</h1>
+                <div className="col-md-5 mx-auto mt-2">
+                    <h3 className="pt-4 pb-2 text-center"><i className="fas fa-table"></i>&nbsp;Details</h3>
+                    <form id="residential-form" method="post" className="needs-validation" action="" noValidate>
+                        <div className="form-group">
+
                             <p>All fields which have the (*) symbol are required to be filled out.</p>
 
                             <BootstrapErrorsProcessingAlert errors={errors} />
 
+                            <BootstrapInput
+                                inputClassName="form-control form-control-lg"
+                                borderColour="border-primary"
+                                error={errors.name}
+                                label="Name (*)"
+                                onChange={onTextChange}
+                                value={name}
+                                name="name"
+                                type="text"
+                            />
 
+                            <BootstrapTextarea
+                                name="description"
+                                borderColour="border-primary"
+                                label="Description (*)"
+                                placeholder="Please describe your concern"
+                                rows="5"
+                                value={description}
+                                helpText=""
+                                onChange={onTextChange}
+                                error={errors.description}
+                            />
 
-                            <div className="form-group">
-                                <button className="btn btn-success btn-lg mt-4 float-right pl-4 pr-4" disabled={isLoading} onClick={onClick}>
-                                    <i className="fas fa-check-circle"></i>&nbsp;Save
-                                </button>
-                                <Link to={`/admin/watch/${slug}`} className="btn btn-orange btn-lg mt-4 float-left pl-4 pr-4">
-                                    <i className="fas fa-arrow-circle-left"></i> Back
-                                </Link>
-                            </div>
+                            <BootstrapSingleSelect
+                                label="District (*)"
+                                name="district"
+                                defaultOptionLabel="Please select the residential district."
+                                options={districtOptions}
+                                value={district}
+                                error={errors.district}
+                                onSelectChange={onSelectChange}
+                                isLoading={isDistrictLoading}
+                            />
 
-                        </form>
-                    </div>
+                            <BootstrapMultipleSelect
+                                borderColour="border-success"
+                                label="Tags"
+                                name="tags"
+                                defaultOptionLabel="Please select the tag."
+                                options={tagOptions}
+                                selectedOptions={tags}
+                                error={errors.tags}
+                                onMultiChange={onMultiChange}
+                                isLoading={isTagLoading}
+                            />
+
+                            <button className="btn btn-success btn-lg mt-4 float-right pl-4 pr-4" disabled={isLoading} onClick={onClick}>
+                                Next&nbsp;<i className="fas fa-arrow-circle-right"></i>
+                            </button>
+                            <Link to={`/admin/watch/${slug}`} className="btn btn-secondary btn-lg mt-4 float-left pl-4 pr-4">
+                                <i className="fas fa-arrow-circle-left"></i>&nbsp;Back
+                            </Link>
+                        </div>
+
+                    </form>
                 </div>
 
             </main>
--- src/containers/watches/admin/update/adminInfoUpdateContainer.js
+++ src/containers/watches/admin/update/adminInfoUpdateContainer.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Scroll from 'react-scroll';
-import * as moment from 'moment';
 
 import AdminWatchInfoUpdateComponent from "../../../../components/watches/admin/update/adminInfoUpdateComponent";
-import { setFlashMessage } from "../../../../actions/flashMessageActions";
-import { validateInput } from "../../../../validators/watchValidator";
-import {
-    RESIDENTIAL_CUSTOMER_TYPE_OF_ID, COMMERCIAL_CUSTOMER_TYPE_OF_ID
-} from '../../../../constants/api';
-import { getTagReactSelectOptions, getPickedTagReactSelectOptions, pullTagList } from "../../../../actions/tagActions";
+import { validateStep2CreateInput } from "../../../../validators/watchValidator";
+import { getAssociateReactSelectOptions } from '../../../../actions/watchActions';
+import { pullDistrictList, getDistrictReactSelectOptions } from '../../../../actions/districtActions';
+import { getAreaCoordinatorReactSelectOptions } from '../../../../actions/areaCoordinatorActions';
+import { pullTagList, getTagReactSelectOptions, getPickedTagReactSelectOptions } from "../../../../actions/tagActions";
 import { putWatch } from "../../../../actions/watchActions";
+import {
+    BASIC_STREET_TYPE_CHOICES,
+    STREET_DIRECTION_CHOICES,
+    RESIDENCE_TYPE_OF,
+    BUSINESS_TYPE_OF,
+    COMMUNITY_CARES_TYPE_OF
+} from "../../../../constants/api";
+
 
 
 class AdminWatchInfoUpdateContainer extends Component {
@@ -26,34 +32,32 @@ class AdminWatchInfoUpdateContainer extends Component {
         // fetch the URL argument as follows.
         const { slug } = this.props.match.params;
 
+        const transcodedTags = getPickedTagReactSelectOptions(this.props.watchDetail.tags, this.props.tagList)
+
         this.state = {
-            errors: {},
-            isLoading: false,
             slug: slug,
             typeOf: this.props.watchDetail.typeOf,
+            tags: transcodedTags,
+            isTagLoading: true,
+            name: this.props.watchDetail.name,
+            description: this.props.watchDetail.description,
+            // associate: localStorage.getItem('nwapp-watch-associate'),
+            // associateOption: localStorageGetObjectItem('nwapp-watch-associateOption'),
+            district: this.props.watchDetail.districtSlug,
+            // districtOption: localStorageGetObjectItem('nwapp-watch-districtOption'),
+            isDistrictLoading: true,
+            errors: {},
         }
 
-        this.getPostData = this.getPostData.bind(this);
+        // Page related.
+        this.onClick = this.onClick.bind(this);
         this.onTextChange = this.onTextChange.bind(this);
         this.onSelectChange = this.onSelectChange.bind(this);
-        this.onClick = this.onClick.bind(this);
-        this.onSuccessCallback = this.onSuccessCallback.bind(this);
-        this.onFailedCallback = this.onFailedCallback.bind(this);
-    }
-
-    /**
-     *  Utility function used to create the `postData` we will be submitting to
-     *  the API; as a result, this function will structure some dictionary key
-     *  items under different key names to support our API web-service's API.
-     */
-    getPostData() {
-        let postData = Object.assign({}, this.state);
-
-
-
-        // Finally: Return our new modified data.
-        console.log("getPostData |", postData);
-        return postData;
+        this.onMultiChange = this.onMultiChange.bind(this);
+        this.onDistrictSuccessCallback = this.onDistrictSuccessCallback.bind(this);
+        this.onDistrictFailureCallback = this.onDistrictFailureCallback.bind(this);
+        this.onTagSuccessCallback = this.onTagSuccessCallback.bind(this);
+        this.onTagFailureCallback = this.onTagFailureCallback.bind(this);
     }
 
     /**
@@ -64,8 +68,25 @@ class AdminWatchInfoUpdateContainer extends Component {
     componentDidMount() {
         window.scrollTo(0, 0);  // Start the page at the top of the page.
 
-        // Fetch all our GUI drop-down options which are populated by the API.
-        this.props.pullTagList(1,1000);
+        // Get our data.
+        const parametersMap = new Map();
+        parametersMap.set("is_archived", 3); // 3 = TRUE | 2 = FALSE
+        parametersMap.set("o", "-created_at");
+        this.props.pullTagList(
+            1,
+            10000,
+            parametersMap,
+            this.onTagSuccessCallback,
+            this.onTagFailureCallback
+        );
+        parametersMap.set("type_of", this.state.typeOf);
+        this.props.pullDistrictList(
+            1,
+            10000,
+            parametersMap,
+            this.onDistrictSuccessCallback,
+            this.onDistrictFailureCallback
+        );
     }
 
     componentWillUnmount() {
@@ -82,14 +103,36 @@ class AdminWatchInfoUpdateContainer extends Component {
      *------------------------------------------------------------
      */
 
-    onSuccessCallback(watch) {
-        this.setState({ errors: {}, isLoading: true, })
-        this.props.setFlashMessage("success", "Watch has been successfully updated.");
-        this.props.history.push("/admin/watch/"+this.state.slug);
+    onDistrictSuccessCallback(response) {
+        console.log(response);
+        this.setState({ isDistrictLoading: false, }, ()=>{
+            console.log("onDistrictSuccessCallback: Successfully loaded the districts list.");
+        });
     }
 
-    onFailedCallback(errors) {
-        this.setState({ errors: errors, isLoading: false, });
+    onDistrictFailureCallback(errors) {
+        this.setState({ errors: errors, isDistrictLoading: false, }, ()=>{
+            console.log("onDistrictSuccessCallback: Failed loading the districts list.");
+        });
+
+        // The following code will cause the screen to scroll to the top of
+        // the page. Please see ``react-scroll`` for more information:
+        // https://github.com/fisshy/react-scroll
+        var scroll = Scroll.animateScroll;
+        scroll.scrollToTop();
+    }
+
+    onTagSuccessCallback(response) {
+        console.log(response);
+        this.setState({ isTagLoading: false, }, ()=>{
+            console.log("onTagSuccessCallback: Successfully loaded the districts list.");
+        });
+    }
+
+    onTagFailureCallback(errors) {
+        this.setState({ errors: errors, isTagLoading: false, }, ()=>{
+            console.log("onTagSuccessCallback: Failed loading the districts list.");
+        });
 
         // The following code will cause the screen to scroll to the top of
         // the page. Please see ``react-scroll`` for more information:
@@ -106,39 +149,49 @@ class AdminWatchInfoUpdateContainer extends Component {
     onTextChange(e) {
         this.setState({
             [e.target.name]: e.target.value,
-        })
+        });
+    }
+
+    onSelectChange(option) {
+        const optionKey = [option.selectName]+"Option";
+        this.setState({
+            [option.selectName]: option.value,
+            optionKey: option,
+        });
+    }
+
+    onMultiChange(...args) {
+        // Extract the select options from the parameter.
+        const selectedOptions = args[0];
+
+        // Set all the tags we have selected to the STORE.
+        this.setState({
+            tags: selectedOptions,
+        });
     }
 
     onClick(e) {
         // Prevent the default HTML form submit code to run on the browser side.
         e.preventDefault();
 
-        // Perform watch-side validation.
-        const { errors, isValid } = validateInput(this.state);
+        // Perform client-side validation.
+        const { errors, isValid } = validateStep2CreateInput(this.state);
 
         // CASE 1 OF 2: Validation passed successfully.
         if (isValid) {
-            this.setState({ errors: {}, isLoading: true, }, ()=>{
-                this.props.putWatch(
-                    this.getPostData(),
-                    this.onSuccessCallback,
-                    this.onFailedCallback
-                );
-            });
+            this.setState({ errors: {}, isLoading: true, })
+            this.props.history.push("/admin/watch/"+this.state.slug);
 
         // CASE 2 OF 2: Validation was a failure.
         } else {
-            this.onFailedCallback(errors);
-        }
-    }
+            this.setState({ errors: errors });
 
-    onSelectChange(option) {
-        const optionKey = [option.selectName].toString()+"Option";
-        this.setState({
-            [option.selectName]: option.value,
-            optionKey: option,
-        });
-        console.log([option.selectName], optionKey, "|",option); // For debugging purposes only.
+            // The following code will cause the screen to scroll to the top of
+            // the page. Please see ``react-scroll`` for more information:
+            // https://github.com/fisshy/react-scroll
+            var scroll = Scroll.animateScroll;
+            scroll.scrollToTop();
+        }
     }
 
     /**
@@ -148,20 +201,32 @@ class AdminWatchInfoUpdateContainer extends Component {
 
     render() {
         const {
-            errors, slug, isLoading, typeOf,
+            slug, tags, isTagLoading, name, description, district, isDistrictLoading, errors
         } = this.state;
+
+        const districtOptions = getDistrictReactSelectOptions(this.props.districtList, "district");
+        const tagOptions = getTagReactSelectOptions(this.props.tagList, "tags");
+
+        // // For debugging purposes only.
+        // console.log("districtOptions", districtOptions);
+        // console.log("tagOptions", tagOptions);
 
         return (
             <AdminWatchInfoUpdateComponent
-                typeOf={typeOf}
-
-                // EVERYTHING ELSE
-                isLoading={isLoading}
                 slug={slug}
+                tags={tags}
+                isTagLoading={isTagLoading}
+                tagOptions={tagOptions}
+                name={name}
+                description={description}
+                district={district}
+                isDistrictLoading={isDistrictLoading}
+                districtOptions={districtOptions}
                 errors={errors}
+                onClick={this.onClick}
                 onTextChange={this.onTextChange}
                 onSelectChange={this.onSelectChange}
-                onClick={this.onClick}
+                onMultiChange={this.onMultiChange}
             />
         );
     }
@@ -171,18 +236,21 @@ const mapStateToProps = function(store) {
     return {
         user: store.userState,
         watchDetail: store.watchDetailState,
+        districtList: store.districtListState,
         tagList: store.tagListState,
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        setFlashMessage: (typeOf, text) => {
-            dispatch(setFlashMessage(typeOf, text))
-        },
-        pullTagList: (page, sizePerPage, map, onSuccessCallback, onFailureCallback) => {
+        pullDistrictList: (page, sizePerPage, map, onSuccessListCallback, onFailureListCallback) => {
             dispatch(
-                pullTagList(page, sizePerPage, map, onSuccessCallback, onFailureCallback)
+                pullDistrictList(page, sizePerPage, map, onSuccessListCallback, onFailureListCallback)
+            )
+        },
+        pullTagList: (page, sizePerPage, map, onSuccessListCallback, onFailureListCallback) => {
+            dispatch(
+                pullTagList(page, sizePerPage, map, onSuccessListCallback, onFailureListCallback)
             )
         },
         putWatch: (data, onSuccessCallback, onFailureCallback) => {
@@ -192,6 +260,7 @@ const mapDispatchToProps = dispatch => {
         },
     }
 }
+
 
 export default connect(
     mapStateToProps,
