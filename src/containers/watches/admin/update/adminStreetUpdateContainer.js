25bc3204de90be413954b1eb8e40f0ef3722925a
bartmika <bart@mikasoftware.com>
2020-01-30 22:40:29 -0500

    Migrating the `street` GUI from the add create 3 page.

--- src/containers/watches/admin/update/adminStreetUpdateContainer.js
+++ src/containers/watches/admin/update/adminStreetUpdateContainer.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Scroll from 'react-scroll';
-import * as moment from 'moment';
 
-import AdminWatchStreetUpdateComponent from "../../../../components/watches/admin/update/adminStreetUpdateComponent";
-import { setFlashMessage } from "../../../../actions/flashMessageActions";
-import { validateInput } from "../../../../validators/watchValidator";
 import {
-    RESIDENTIAL_CUSTOMER_TYPE_OF_ID, COMMERCIAL_CUSTOMER_TYPE_OF_ID
-} from '../../../../constants/api';
-import { getTagReactSelectOptions, getPickedTagReactSelectOptions, pullTagList } from "../../../../actions/tagActions";
-import { putWatch } from "../../../../actions/watchActions";
+    validateResidentialStep3Input, validateModalSaveInput
+} from "../../../../validators/watchValidator";
+import AdminWatchCreateStep3Component from "../../../../components/watches/admin/create/adminCreateStep3Component";
+import {
+    localStorageGetObjectItem, localStorageSetObjectOrArrayItem, localStorageGetArrayItem
+} from '../../../../helpers/localStorageUtility';
+import { getAssociateReactSelectOptions } from '../../../../actions/watchActions';
+import { getDistrictReactSelectOptions } from '../../../../actions/districtActions';
+import { getAreaCoordinatorReactSelectOptions } from '../../../../actions/areaCoordinatorActions';
+import { getTagReactSelectOptions } from "../../../../actions/tagActions";
+import { BASIC_STREET_TYPE_CHOICES, STREET_DIRECTION_CHOICES } from "../../../../constants/api";
 
 
 class AdminWatchStreetUpdateContainer extends Component {
@@ -21,39 +24,31 @@ class AdminWatchStreetUpdateContainer extends Component {
 
     constructor(props) {
         super(props);
-
-        // Since we are using the ``react-routes-dom`` library then we
-        // fetch the URL argument as follows.
-        const { slug } = this.props.match.params;
-
         this.state = {
+            streetMembership: localStorageGetArrayItem('nwapp-watch-streetMembership'),
             errors: {},
-            isLoading: false,
-            slug: slug,
-            typeOf: this.props.watchDetail.typeOf,
+            streetNumberStart: "",
+            streetNumberFinish: "",
+            streetName: "",
+            streetType: "",
+            streetTypeOption: localStorageGetObjectItem('nwapp-watch-streetTypeOption'),
+            streetTypeOther: "",
+            streetDirection: "",
+            streetDirectionOption: localStorageGetObjectItem('nwapp-watch-streetDirectionOption'),
+            showModal: false, // Variable used to indicate if the modal should appear.
         }
 
-        this.getPostData = this.getPostData.bind(this);
+        // Page related.
+        this.onClick = this.onClick.bind(this);
         this.onTextChange = this.onTextChange.bind(this);
         this.onSelectChange = this.onSelectChange.bind(this);
-        this.onClick = this.onClick.bind(this);
-        this.onSuccessCallback = this.onSuccessCallback.bind(this);
-        this.onFailedCallback = this.onFailedCallback.bind(this);
-    }
+        this.onMultiChange = this.onMultiChange.bind(this);
 
-    /**
-     *  Utility function used to create the `postData` we will be submitting to
-     *  the API; as a result, this function will structure some dictionary key
-     *  items under different key names to support our API web-service's API.
-     */
-    getPostData() {
-        let postData = Object.assign({}, this.state);
-
-
-
-        // Finally: Return our new modified data.
-        console.log("getPostData |", postData);
-        return postData;
+        // Modal related.
+        this.onAddClick = this.onAddClick.bind(this);
+        this.onRemoveClick = this.onRemoveClick.bind(this);
+        this.onSaveClick = this.onSaveClick.bind(this);
+        this.onCloseClick = this.onCloseClick.bind(this);
     }
 
     /**
@@ -63,9 +58,6 @@ class AdminWatchStreetUpdateContainer extends Component {
 
     componentDidMount() {
         window.scrollTo(0, 0);  // Start the page at the top of the page.
-
-        // Fetch all our GUI drop-down options which are populated by the API.
-        this.props.pullTagList(1,1000);
     }
 
     componentWillUnmount() {
@@ -82,21 +74,6 @@ class AdminWatchStreetUpdateContainer extends Component {
      *------------------------------------------------------------
      */
 
-    onSuccessCallback(watch) {
-        this.setState({ errors: {}, isLoading: true, })
-        this.props.setFlashMessage("success", "Watch has been successfully updated.");
-        this.props.history.push("/admin/watch/"+this.state.slug);
-    }
-
-    onFailedCallback(errors) {
-        this.setState({ errors: errors, isLoading: false, });
-
-        // The following code will cause the screen to scroll to the top of
-        // the page. Please see ``react-scroll`` for more information:
-        // https://github.com/fisshy/react-scroll
-        var scroll = Scroll.animateScroll;
-        scroll.scrollToTop();
-    }
 
     /**
      *  Event handling functions
@@ -107,38 +84,172 @@ class AdminWatchStreetUpdateContainer extends Component {
         this.setState({
             [e.target.name]: e.target.value,
         })
+        localStorage.setItem('nwapp-watch-'+[e.target.name], e.target.value);
+    }
+
+    onSelectChange(option) {
+        const optionKey = [option.selectName]+"Option";
+        this.setState({
+            [option.selectName]: option.value,
+            optionKey: option,
+        });
+        localStorage.setItem('nwapp-watch-'+[option.selectName], option.value);
+        localStorageSetObjectOrArrayItem('nwapp-watch-'+optionKey, option);
+        // console.log([option.selectName], optionKey, "|", this.state); // For debugging purposes only.
+    }
+
+    onMultiChange(...args) {
+        // Extract the select options from the parameter.
+        const selectedOptions = args[0];
+
+        // Set all the tags we have selected to the STORE.
+        this.setState({
+            tags: selectedOptions,
+        });
+
+        // // Set all the tags we have selected to the STORAGE.
+        const key = 'nwapp-watch-' + args[1].name;
+        localStorageSetObjectOrArrayItem(key, selectedOptions);
     }
 
     onClick(e) {
         // Prevent the default HTML form submit code to run on the browser side.
         e.preventDefault();
 
-        // Perform watch-side validation.
-        const { errors, isValid } = validateInput(this.state);
+        // Perform client-side validation.
+        const { errors, isValid } = validateResidentialStep3Input(this.state);
 
         // CASE 1 OF 2: Validation passed successfully.
         if (isValid) {
-            this.setState({ errors: {}, isLoading: true, }, ()=>{
-                this.props.putWatch(
-                    this.getPostData(),
-                    this.onSuccessCallback,
-                    this.onFailedCallback
-                );
-            });
+            this.setState({ errors: {}, isLoading: true, });
+            this.props.history.push("/admin/watches/step-4-create");
 
         // CASE 2 OF 2: Validation was a failure.
         } else {
-            this.onFailedCallback(errors);
+            this.setState({ errors: errors, });
+
+            // The following code will cause the screen to scroll to the top of
+            // the page. Please see ``react-scroll`` for more information:
+            // https://github.com/fisshy/react-scroll
+            var scroll = Scroll.animateScroll;
+            scroll.scrollToTop();
         }
     }
 
-    onSelectChange(option) {
-        const optionKey = [option.selectName].toString()+"Option";
+    onAddClick(e) {
+        e.preventDefault();  // Prevent the default HTML form submit code to run on the browser side.
         this.setState({
-            [option.selectName]: option.value,
-            optionKey: option,
-        });
-        console.log([option.selectName], optionKey, "|",option); // For debugging purposes only.
+            showModal: true,
+            errors: {},
+        });  // Load the modal.
+    }
+
+    onRemoveClick(streetAddress) {
+        const streetMembership = this.state.streetMembership;
+        for (let i = 0; i < streetMembership.length; i++) {
+            let row = streetMembership[i];
+
+            // // For debugging purposes only.
+            // console.log(row);
+            // console.log(streetAddress);
+
+            if (row.streetAddress === streetAddress) {
+                //
+                // Special thanks: https://flaviocopes.com/how-to-remove-item-from-array/
+                //
+                const filteredItems = streetMembership.slice(
+                    0, i
+                ).concat(
+                    streetMembership.slice(
+                        i + 1, streetMembership.length
+                    )
+                )
+
+                // Update our state with our NEW ARRAY which no longer has
+                // the item we deleted.
+                this.setState({
+                    streetMembership: filteredItems
+                });
+
+                // Save our table data.
+                localStorageSetObjectOrArrayItem("nwapp-watch-streetMembership", filteredItems);
+
+                // Terminate our for-loop.
+                return;
+            }
+        }
+    }
+
+    onSaveClick(e) {
+        // Prevent the default HTML form submit code to run on the browser side.
+        e.preventDefault();
+
+        // Perform client-side validation.
+        const { errors, isValid } = validateModalSaveInput(this.state);
+
+        // CASE 1 OF 2: Validation passed successfully.
+        if (isValid) {
+
+            // Generate our new address.
+            const actualStreetType = this.state.streetType === "Other" ? this.state.streetTypeOther : this.state.streetType;
+            let streetAddress = this.state.streetName+" "+actualStreetType;
+            if (this.state.streetDirection) {
+                streetAddress += " " + this.state.streetDirection;
+            }
+            streetAddress += " from "+this.state.streetNumberStart+" to "+this.state.streetNumberFinish;
+
+            // Append our array.
+            let a = this.state.streetMembership.slice(); //creates the clone of the state
+            a.push({
+                streetAddress: streetAddress,
+                streetNumberStart: this.state.streetNumberStart,
+                streetNumberFinish: this.state.streetNumberFinish,
+                streetName: this.state.streetName,
+                streetType: actualStreetType,
+                streetDirection: this.state.streetDirection,
+            });
+
+            // Update the state.
+            this.setState({
+                showModal: false,
+                errors: {},
+                streetMembership: a,
+                streetNumberStart: "", // Clear fields.
+                streetNumberFinish: "",
+                streetName: "",
+                streetType: "",
+                streetTypeOther: "",
+                streetDirection: "",
+            })
+
+            // Save our table data.
+            localStorageSetObjectOrArrayItem("nwapp-watch-streetMembership", a);
+
+        // CASE 2 OF 2: Validation was a failure.
+        } else {
+            this.setState({
+                errors: errors
+            })
+
+            // The following code will cause the screen to scroll to the top of
+            // the page. Please see ``react-scroll`` for more information:
+            // https://github.com/fisshy/react-scroll
+            var scroll = Scroll.animateScroll;
+            scroll.scrollToTop();
+        }
+    }
+
+    onCloseClick() {
+        this.setState({
+            showModal: false,
+            errors: {},
+            streetNumberStart: "", // Clear fields.
+            streetNumberFinish: "",
+            streetName: "",
+            streetType: "",
+            streetTypeOther: "",
+            streetDirection: ""
+        })
     }
 
     /**
@@ -148,20 +259,74 @@ class AdminWatchStreetUpdateContainer extends Component {
 
     render() {
         const {
-            errors, slug, isLoading, typeOf,
+            // Page related.
+            tags, name, description, associate, district, primaryAreaCoordinator, secondaryAreaCoordinator, streetMembership, errors,
+
+            // Modal relate.
+            streetNumberStart, streetNumberFinish, streetName, streetType, streetTypeOther, streetDirection, showModal,
         } = this.state;
 
-        return (
-            <AdminWatchStreetUpdateComponent
-                typeOf={typeOf}
+        const associateListObject = {
+            results: [
+                {'slug': 'bob-page', 'name': 'Bob Page'},
+                {'slug': 'jc-denton', 'name': 'JC Denton'},
+                {'slug': 'paul-denton', 'name': 'Paul Denton'},
+                {'slug': 'gunter-herman', 'name': 'Gunter Herman'}
+            ]
+        }; // TODO: REPLACTE WITH API DATA.
 
-                // EVERYTHING ELSE
-                isLoading={isLoading}
-                slug={slug}
+        const districtListObject = {
+            results: [
+                {'slug': 'wanchai', 'name': 'Wanchai Market'},
+                {'slug': 'versalife', 'name': 'VersaLife'},
+                {'slug': 'battery-park', 'name': 'Battery Park'},
+                {'slug': 'area-51', 'name': 'Area 51'}
+            ]
+        }; // TODO: REPLACTE WITH API DATA.
+
+        const areaCoordinatorListObject = {
+            results: [
+                {'slug': 'tracer-tong', 'name': 'Tracer Tong'},
+                {'slug': 'icarus', 'name': 'Icarus'},
+                {'slug': 'datalus', 'name': 'Datalus'},
+            ]
+        }; // TODO: REPLACTE WITH API DATA.
+
+        const tagOptions = getTagReactSelectOptions(this.state.tagsData, "tags");
+
+        return (
+            <AdminWatchCreateStep3Component
+                tags={tags}
+                tagOptions={tagOptions}
+                name={name}
+                description={description}
+                associate={associate}
+                associateOptions={getAssociateReactSelectOptions(associateListObject)}
+                district={district}
+                districtOptions={getDistrictReactSelectOptions(districtListObject)}
+                primaryAreaCoordinator={primaryAreaCoordinator}
+                primaryAreaCoordinatorOptions={getAreaCoordinatorReactSelectOptions(areaCoordinatorListObject, "primaryAreaCoordinator")}
+                secondaryAreaCoordinator={secondaryAreaCoordinator}
+                secondaryAreaCoordinatorOptions={getAreaCoordinatorReactSelectOptions(areaCoordinatorListObject, "secondaryAreaCoordinator")}
+                streetMembership={streetMembership}
                 errors={errors}
+                onClick={this.onClick}
                 onTextChange={this.onTextChange}
                 onSelectChange={this.onSelectChange}
-                onClick={this.onClick}
+                onMultiChange={this.onMultiChange}
+                showModal={showModal}
+                streetNumberStart={streetNumberStart}
+                streetNumberFinish={streetNumberFinish}
+                streetName={streetName}
+                streetType={streetType}
+                streetTypeOptions={BASIC_STREET_TYPE_CHOICES}
+                streetTypeOther={streetTypeOther}
+                streetDirection={streetDirection}
+                streetDirectionOptions={STREET_DIRECTION_CHOICES}
+                onAddClick={this.onAddClick}
+                onRemoveClick={this.onRemoveClick}
+                onSaveClick={this.onSaveClick}
+                onCloseClick={this.onCloseClick}
             />
         );
     }
@@ -170,28 +335,13 @@ class AdminWatchStreetUpdateContainer extends Component {
 const mapStateToProps = function(store) {
     return {
         user: store.userState,
-        watchDetail: store.watchDetailState,
-        tagList: store.tagListState,
     };
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        setFlashMessage: (typeOf, text) => {
-            dispatch(setFlashMessage(typeOf, text))
-        },
-        pullTagList: (page, sizePerPage, map, onSuccessCallback, onFailureCallback) => {
-            dispatch(
-                pullTagList(page, sizePerPage, map, onSuccessCallback, onFailureCallback)
-            )
-        },
-        putWatch: (data, onSuccessCallback, onFailureCallback) => {
-            dispatch(
-                putWatch(data, onSuccessCallback, onFailureCallback)
-            )
-        },
-    }
+    return {}
 }
+
 
 export default connect(
     mapStateToProps,
