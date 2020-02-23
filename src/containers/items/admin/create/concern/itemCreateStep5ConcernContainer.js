9f64a71a0f5cfc520099ca21201adbffd967b4b0
bartmika <bart@mikasoftware.com>
2020-02-22 23:03:44 -0500

    Implemented concern code.

--- src/containers/items/admin/create/concern/itemCreateStep5ConcernContainer.js
+++ src/containers/items/admin/create/concern/itemCreateStep5ConcernContainer.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Scroll from 'react-scroll';
+// import * as moment from 'moment';
 
 import ItemCreateStep5ConcernComponent from "../../../../../components/items/admin/create/concern/itemCreateStep5ConcernComponent";
 import {
-    localStorageGetObjectItem, localStorageGetDateItem, localStorageGetArrayItem, localStorageGetIntegerItem
+    localStorageGetObjectItem,
+    localStorageGetDateItem,
+    localStorageGetArrayItem,
+    localStorageGetIntegerItem
 } from '../../../../../helpers/localStorageUtility';
 import { setFlashMessage } from "../../../../../actions/flashMessageActions";
-import {
-    INCIDENT_ITEM_TYPE_OF,
-    EVENT_ITEM_TYPE_OF,
-    CONCERN_ITEM_TYPE_OF,
-    INFORMATION_ITEM_TYPE_OF
-} from "../../../../../constants/api";
+import { postItem } from "../../../../../actions/itemActions";
 
 
 class ItemCreateStep5Container extends Component {
@@ -24,67 +23,47 @@ class ItemCreateStep5Container extends Component {
     constructor(props) {
         super(props);
 
-        // Extract the type of container.
-        const typeOf = parseInt(localStorage.getItem("nwapp-item-create-typeOf"));
-        let returnURL;
-        if (typeOf === INCIDENT_ITEM_TYPE_OF) {
-            returnURL = "/item/add/step-2-incident";
-        } else if (typeOf === EVENT_ITEM_TYPE_OF) {
-            returnURL = "/item/add/step-2-event";
-        } else if (typeOf === CONCERN_ITEM_TYPE_OF) {
-            returnURL = "/item/add/step-2-concern";
-        } else if (typeOf === INFORMATION_ITEM_TYPE_OF) {
-            returnURL = "/item/add/step-2-information";
-        }
-
         // Set the state.
         this.state = {
-            typeOf: typeOf,
-            returnURL: returnURL,
+            // STEP 2
+            typeOf: localStorageGetIntegerItem("nwapp-item-create-typeOf"),
 
-            // Concern Type
-            concernTitle: localStorage.getItem("nwapp-item-create-concern-title"),
-            concernDescription: localStorage.getItem("nwapp-item-create-concern-description"),
-            concernLocation: localStorage.getItem("nwapp-item-create-concern-location"),
-            concernPhotos: localStorageGetArrayItem("nwapp-item-create-concern-photos"),
+            // STEP 3
             category:localStorage.getItem("nwapp-item-create-concern-category"),
             categoryOption: localStorageGetObjectItem('nwapp-item-create-concern-categoryOption'),
             categoryOther: localStorage.getItem("nwapp-item-create-concern-categoryOther"),
 
-            // Event
-            eventTitle: localStorage.getItem("nwapp-item-create-event-title"),
-            eventTypeOf: parseInt(localStorage.getItem("nwapp-item-create-event-eventTypeOf")),
-            eventTypeOfOption: localStorageGetObjectItem('nwapp-item-create-event-eventTypeOfOption'),
-            eventTypeOfOther: localStorage.getItem("nwapp-item-create-event-eventTypeOfOther"),
-            eventPrettyEventTypeOf: localStorage.getItem('nwapp-item-create-event-pretty-event-type'),
-            eventDate: localStorageGetDateItem("nwapp-item-create-event-date"),
-            eventDescription: localStorage.getItem("nwapp-item-create-event-description"),
-            logoPhoto: localStorageGetArrayItem("nwapp-item-create-event-logoPhoto"),
-            galleryPhotos: localStorageGetArrayItem("nwapp-item-create-event-galleryPhotos"),
-            shownToWhom: localStorageGetIntegerItem("nwapp-item-create-event-shownToWhom"),
-            shownToWhomLabel: localStorage.getItem("nwapp-item-create-event-shownToWhom-label"),
-            canBePostedOnSocialMedia: localStorageGetIntegerItem("nwapp-item-create-event-canBePostedOnSocialMedia"),
-            canBePostedOnSocialMediaLabel: localStorage.getItem("nwapp-item-create-event-canBePostedOnSocialMedia-label"),
+            // STEP 4
+            title: localStorage.getItem("nwapp-item-create-concern-title"),
+            description: localStorage.getItem("nwapp-item-create-concern-description"),
+            location: localStorage.getItem("nwapp-item-create-concern-location"),
+            // photos: localStorageGetArrayItem("nwapp-item-create-concern-photos"),
 
-            // Incident
-            title: localStorage.getItem("nwapp-item-create-incident-title"),
-            date: localStorageGetDateItem("nwapp-item-create-incident-date"),
-            description: localStorage.getItem("nwapp-item-create-incident-description"),
-            location: localStorage.getItem("nwapp-item-create-incident-location"),
-            photos: localStorageGetArrayItem("nwapp-item-create-incident-photos"),
-
-            // Information
-            informationDescription: localStorage.getItem("nwapp-item-create-information-description"),
-
+            // COMMON
             errors: {},
             isLoading: false
         }
 
         // Set the functions.
-        this.onTextChange = this.onTextChange.bind(this);
         this.onClick = this.onClick.bind(this);
         this.onSuccessfulSubmissionCallback = this.onSuccessfulSubmissionCallback.bind(this);
         this.onFailedSubmissionCallback = this.onFailedSubmissionCallback.bind(this);
+    }
+
+    /**
+     *  Utility function used to create the `postData` we will be submitting to
+     *  the API; as a result, this function will structure some dictionary key
+     *  items under different key names to support our API web-service's API.
+     */
+    getPostData() {
+        let postData = Object.assign({}, this.state);
+
+        // const dateMoment = moment(this.state.date);
+        // postData.date = dateMoment.format("YYYY-MM-DD")
+
+        // Finally: Return our new modified data.
+        console.log("getPostData |", postData);
+        return postData;
     }
 
     /**
@@ -133,18 +112,17 @@ class ItemCreateStep5Container extends Component {
      *------------------------------------------------------------
      */
 
-    onTextChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
-
     onClick(e) {
         // Prevent the default HTML form submit code to run on the browser side.
         e.preventDefault();
 
-        // Simply submit.
-        this.onSuccessfulSubmissionCallback();
+        // Once our state has been validated `client-side` then we will
+        // make an API request with the server to create our new production.
+        this.props.postItem(
+            this.getPostData(),
+            this.onSuccessfulSubmissionCallback,
+            this.onFailedSubmissionCallback
+        );
     }
 
 
@@ -189,7 +167,6 @@ class ItemCreateStep5Container extends Component {
                 typeOf={typeOf}
                 returnURL={returnURL}
                 errors={errors}
-                onTextChange={this.onTextChange}
                 onClick={this.onClick}
 
                 concernTitle={concernTitle}
@@ -211,7 +188,10 @@ const mapDispatchToProps = dispatch => {
     return {
         setFlashMessage: (typeOf, text) => {
             dispatch(setFlashMessage(typeOf, text))
-        }
+        },
+        postItem: (postData, successCallback, failedCallback) => {
+            dispatch(postItem(postData, successCallback, failedCallback))
+        },
     }
 }
 
