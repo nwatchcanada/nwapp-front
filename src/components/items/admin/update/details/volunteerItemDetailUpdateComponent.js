bf5e83fcb8b55bceae4ada1101749fdba8a0a6c6
bartmika <bart@mikasoftware.com>
2020-03-01 23:44:39 -0500

    Implemented volunteer  updates. https://github.com/nwatchcanada/nwapp-front/issues/263

---
+++ src/components/items/admin/update/details/volunteerItemDetailUpdateComponent.js
@@ -0,0 +1,100 @@
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+import { BootstrapErrorsProcessingAlert } from "../../../../bootstrap/bootstrapAlert";
+import { BootstrapPageLoadingAnimation } from "../../../../bootstrap/bootstrapPageLoadingAnimation";
+import { BootstrapDatePicker } from "../../../../bootstrap/bootstrapDatePicker";
+import { BootstrapInput } from "../../../../bootstrap/bootstrapInput";
+import { BootstrapSingleSelect } from "../../../../bootstrap/bootstrapSingleSelect";
+import { BootstrapRadio } from "../../../../bootstrap/bootstrapRadio";
+import { BootstrapTextarea } from "../../../../bootstrap/bootstrapTextarea";
+import { BootstrapSingleImageUploadAndPreview } from "../../../../bootstrap/bootstrapSingleImageUploadAndPreview";
+import { BootstrapMultipleImageUploadAndPreview } from "../../../../bootstrap/bootstrapMultipleImageUploadAndPreview";
+import { OTHER_INCIDENT_TYPE_OF } from "../../../../../constants/api";
+
+
+export default class VolunteerItemDetailUpdateComponent extends Component {
+    render() {
+        const {
+            slug,
+            errors,
+            onTextChange,
+            onSelectChange,
+            isLoading,
+            onClick,
+            isItemTypeLoading,
+
+            description, externalUrl,
+        } = this.props;
+
+        return (
+            <main id="main" role="main">
+                <BootstrapPageLoadingAnimation isLoading={isLoading} />
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item">
+                           <Link to="/dashboard"><i className="fas fa-tachometer-alt"></i>&nbsp;Dashboard</Link>
+                        </li>
+                        <li className="breadcrumb-item" aria-current="page">
+                            <Link to="/admin/items"><i className="fas fa-map-pin"></i>&nbsp;Items</Link>
+                        </li>
+                        <li className="breadcrumb-item" aria-current="page">
+                            <Link to={`/admin/item/${slug}`}><i className="fas fa-map-pin"></i>&nbsp;Details</Link>
+                        </li>
+                        <li className="breadcrumb-item active" aria-current="page">
+                            <i className="fas fa-map-pin"></i>&nbsp;Edit
+                        </li>
+                    </ol>
+                </nav>
+
+                <h1><i className="fas fa-edit"></i>&nbsp;Edit Volunteer Item</h1>
+
+                <div className="row">
+                    <div className="col-md-5 mx-auto mt-2">
+                        <form>
+                            <h1><i className="fas fa-sign"></i>&nbsp;Details Form</h1>
+                            <p>All fields which have the (*) symbol are required to be filled out.</p>
+
+                            <BootstrapErrorsProcessingAlert errors={errors} />
+
+                            <BootstrapTextarea
+                                name="description"
+                                borderColour="border-primary"
+                                label="Description (*)"
+                                placeholder="Please describe your incident"
+                                rows="5"
+                                value={description}
+                                helpText=""
+                                onChange={onTextChange}
+                                error={errors.description}
+                            />
+
+                            <BootstrapInput
+                                inputClassName="form-control form-control-lg"
+                                borderColour="border-success"
+                                error={errors.externalUrl}
+                                label="External URL"
+                                onChange={onTextChange}
+                                value={externalUrl}
+                                name="externalUrl"
+                                type="text"
+                            />
+
+                            <div className="form-group">
+                                <button className="btn btn-success btn-lg mt-4 float-right pl-4 pr-4" disabled={isLoading} onClick={ (event)=>{onClick(event)} }>
+                                    <i className="fas fa-check-circle"></i>&nbsp;Save
+                                </button>
+                                <Link to={`/admin/item/${slug}`} className="btn btn-secondary btn-lg mt-4 float-left pl-4 pr-4">
+                                    <i className="fas fa-arrow-circle-left"></i> Back
+                                </Link>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+
+            </main>
+        );
+    }
+}
