5575b38647dce62658017bba19a0590fcc030471
bartmika <bart@mikasoftware.com>
2020-03-14 23:00:42 -0400

    Initial commit for action incident.

---
+++ src/components/taskItems/admin/operations/actionIncident/step3Component.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
+import 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import filterFactory, { selectFilter } from 'react-bootstrap-table2-filter';
+// import overlayFactory from 'react-bootstrap-table2-overlay';
+
+import { BootstrapPageLoadingAnimation } from "../../../../bootstrap/bootstrapPageLoadingAnimation";
+import { FlashMessageComponent } from "../../../../flashMessageComponent";
+
+
+export default class ActionIncidentTaskStep3Component extends Component {
+    render() {
+        const {
+            uuid, associates, isLoading, errors, hasNext, onNextClick,
+            hasPrevious, onPreviousClick, onAssociateClick
+        } = this.props;
+        const hasNoAssociates = associates.length <= 0;
+        return (
+            <div>
+                <BootstrapPageLoadingAnimation isLoading={isLoading} />
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item">
+                           <Link to="/dashboard"><i className="fas fa-tachometer-alt"></i>&nbsp;Dashboard</Link>
+                        </li>
+                        <li className="breadcrumb-item" aria-current="page">
+                            <Link to={`/admin/tasks`}><i className="fas fa-tasks"></i>&nbsp;Tasks</Link>
+                        </li>
+                        <li className="breadcrumb-item active" aria-current="page">
+                            <i className="fas fa-thumbtack"></i>&nbsp;Action Incident
+                        </li>
+                    </ol>
+                </nav>
+
+                <h1><i className="fas fa-thumbtack"></i>&nbsp;Action Incident</h1>
+
+                <div className="row">
+                    <div className="step-navigation">
+                        <div id="step-1" className="st-grey">
+                            <Link to={`/admin/task/3/${uuid}/step-1`}>
+                                <span className="num">1.</span><span className="">Info</span>
+                            </Link>
+                        </div>
+                        <div id="step-2" className="st-grey">
+                            <Link to={`/admin/task/3/${uuid}/step-2`}>
+                                <span className="num">2.</span><span className="">Search</span>
+                            </Link>
+                        </div>
+                        <div id="step-3" className="st-grey active">
+                            <strong>
+                                <span className="num">3.</span><span className="">Selection</span>
+                            </strong>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2>
+                            <i className="fas fa-list"></i>&nbsp;Search Results
+                        </h2>
+
+                        {hasNoAssociates
+                            ?<div className="jumbotron">
+                                <h1 className="display-4">No Results Found</h1>
+                                <p className="lead">It appears nothing was found for your search results. Please try again by clicking below.</p>
+
+                                <p className="lead">
+                                    <Link className="btn btn-primary btn-lg" to={`/admin/task/3/${uuid}/step-2`}>Try Again&nbsp;<i class="fas fa-chevron-right"></i></Link>
+                                </p>
+                            </div>
+                            :<div className="card-group row">
+                                {associates && associates.map(
+                                    (associate) => <CardComponent associate={associate} key={associate.slug} isLoading={isLoading} onAssociateClick={onAssociateClick} />)
+                                }
+                            </div>
+                        }
+
+                        <div className="float-right">
+                            {hasPrevious &&
+                                <Link onClick={onPreviousClick}><i class="fas fa-arrow-circle-left"></i>&nbsp;Previous</Link>
+                            }&nbsp;&nbsp;
+                            {hasNext &&
+                                <Link onClick={onNextClick}>Next&nbsp;<i class="fas fa-arrow-circle-right"></i></Link>
+                            }
+                        </div>
+
+                        <div className="col-md-12 text-center">
+                            <Link to={`/admin/task/3/${uuid}/step-2`} className="btn btn-orange btn-lg mt-4 pl-4 pr-4">
+                                <i className="fas fa-arrow-circle-left"></i>&nbsp;Back
+                            </Link>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+class CardComponent extends Component {
+    render() {
+        const { associate, isLoading, onAssociateClick } = this.props;
+        return (
+            <div className="col-sm-3" id={associate.slug}>
+                <div className="card bg-light">
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            <Link to={`/admin/area-coordinator/${associate.slug}`}>
+                                {associate.typeOf === 3 &&
+                                    <strong><i className="fas fa-building"></i>&nbsp;{associate.organizationName}</strong>
+                                }
+                                {associate.typeOf === 2 &&
+                                    <strong><i className="fas fa-home"></i>&nbsp;{associate.firstName}&nbsp;{associate.lastName}</strong>
+                                }
+                                {associate.typeOf === 1 &&
+                                    <strong><i className="fas fa-home"></i>&nbsp;{associate.firstName}&nbsp;{associate.lastName}</strong>
+                                }
+                            </Link>
+                        </h5>
+                        <p className="card-text">
+                            {associate.streetAddress}<br />
+                            {associate.city}, {associate.province}, {associate.postalCode}<br />
+                            <a href={`email:${associate.email}`}>{associate.email}</a><br />
+                            <a href={`tel:${associate.primaryPhoneE164}`}>{associate.primaryPhoneNational}</a>
+                        </p>
+                        <button className="btn btn-primary btn-lg btn-block" disabled={isLoading} onClick={(event, slug)=>{ onAssociateClick(event, associate.slug) }}>
+                            Select&nbsp;<i class="fas fa-chevron-right"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/containers/taskItems/admin/operations/actionIncident/step1Container.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import ActionIncidentTaskStep1Component from "../../../../../components/taskItems/admin/operations/actionIncident/step1Component";
+import { clearFlashMessage } from "../../../../../actions/flashMessageActions";
+
+
+class ActionIncidentTaskStep1Container extends Component {
+    /**
+     *  Initializer & Utility
+     *------------------------------------------------------------
+     */
+
+    constructor(props) {
+        super(props);
+
+        // Since we are using the ``react-routes-dom`` library then we
+        // fetch the URL argument as follows.
+        const { uuid } = this.props.match.params;
+
+        // Update state.
+        this.state = {
+            uuid: uuid,
+        }
+
+        this.onClick = this.onClick.bind(this);
+    }
+
+    /**
+     *  Component Life-cycle Management
+     *------------------------------------------------------------
+     */
+
+     componentDidMount() {
+         window.scrollTo(0, 0);  // Start the page at the top of the page.
+     }
+
+     componentWillUnmount() {
+         // This code will fix the "ReactJS & Redux: Can't perform a React state
+         // update on an unmounted component" issue as explained in:
+         // https://stackoverflow.com/a/53829700
+         this.setState = (state,callback)=>{
+             return;
+         };
+
+         // Clear any and all flash messages in our queue to be rendered.
+         this.props.clearFlashMessage();
+     }
+
+    /**
+     *  API callback functions
+     *------------------------------------------------------------
+     */
+
+    onSuccessfulSubmissionCallback(profile) {
+        console.log(profile);
+    }
+
+    onFailedSubmissionCallback(errors) {
+        console.log(errors);
+    }
+
+    /**
+     *  Event handling functions
+     *------------------------------------------------------------
+     */
+
+    onClick(e) {
+        // Prevent the default HTML form submit code to run on the browser side.
+        e.preventDefault();
+        this.props.history.push("/admin/task/3/"+this.state.uuid+"/step-2");
+    }
+
+    /**
+     *  Main render function
+     *------------------------------------------------------------
+     */
+
+    render() {
+        return (
+            <ActionIncidentTaskStep1Component
+                urlArgument={this.state.urlArgument}
+                uuid={this.state.uuid}
+                onBack={this.onBack}
+                onClick={this.onClick}
+                flashMessage={this.props.flashMessage}
+            />
+        );
+    }
+}
+
+const mapStateToProps = function(store) {
+    return {
+        user: store.userState,
+        flashMessage: store.flashMessageState,
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        clearFlashMessage: () => {
+            dispatch(clearFlashMessage())
+        }
+    }
+}
+
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ActionIncidentTaskStep1Container);
